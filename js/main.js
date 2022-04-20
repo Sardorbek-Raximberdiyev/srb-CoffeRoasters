b18fef1bc2bf22f2dc547596647db39616c61001
Sardorbek-Raximberdiyev <sardorraximberdiyev9@gmail.com>
2022-04-20 22:35:33 +0500

    Add pasitoin styles

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const createleftlistchilddn = document.querySelector('.create-left-list-child-dn');
+
+createleftlistchilddn.addEventListener('change', function () {
+
+});
+
+
+// const tunbtn = document.querySelector('.tun-btn');
+// const kunbtn = document.querySelector('.kun-btn');
+
+
+// tunbtn.addEventListener('click', function () {
+//   document.body.classList.add('dark-more');
+// });
